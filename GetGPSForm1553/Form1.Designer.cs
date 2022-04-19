602f1eb61693c10e3911575c5ff360f66017dea5
Lu Yang <ly1985618@163.com>
2022-04-19 13:49:55 +0800

    初始提交

---
+++ GetGPSForm1553/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GetGPSForm1553
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 19);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(379, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(11, 46);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(459, 202);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 253);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Extractor";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(91, 253);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(379, 29);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 291);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ GetGPSForm1553/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GetGPSForm1553
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string path;
+        public string readText;
+        public int substringlength = 0;
+        public int count = 0;
+        public int proc_count = 0;
+        public int total_count = 0;
+        public string keystring = "5-T-22-0";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            textBox1.Text = openFileDialog1.FileName;
+                            path = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
+                            StreamReader m_streamReader = new StreamReader(openFileDialog1.FileName, System.Text.Encoding.Default);
+
+                            string filePath = openFileDialog1.FileName;
+
+                            readText = File.ReadAllText(filePath);
+                            total_count = readText.Length;
+                            richTextBox1.Text ="数据文件加载完毕，共"+ total_count.ToString()+"字节！\n";
+                            m_streamReader.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(path + "\\" + "提取结果" + System.DateTime.Now.Ticks.ToString() + ".txt", false, Encoding.Default);
+
+            richTextBox1.Text += "开始提取PVT数据...\n";
+            progressBar1.Maximum = total_count;
+            string s_time;
+            string s_pv;
+            string s_px;
+            string s_py;
+            string s_pz;
+            string s_vx;
+            string s_vy;
+            string s_vz;
+            substringlength = readText.IndexOf(keystring);
+            //string tmmss = "5-T-22-0     DATA 0655 C9B5 0039 FEFF 0617 3717 A5F5 0437 6425 3C31 82F0 82F6 FB01 5EEB 83FE B0C5 C2FB DEEE 7F00 0000 042D BA6C 0218 9ABE 22CD 78C0 04AE 0337 2101 8B7C 26BB 32C5      STA2 2800";
+
+            //substringlength = tmmss.IndexOf(keystring);
+            //s_pv = tmmss.Substring(substringlength + keystring.Length + 32, 10);
+            //richTextBox1.Text += s_pv.ToString() + "\n";
+
+            //CMD2 2EC0-->5-T-22-0     DATA 0655 C9B5 0039 FEFF 0617 3717 A5F5 0437 6425 3C31 82F0 82F6 FB01 5EEB 83FE B0C5 
+            //                                           C2FB DEEE 7F00 0000 042D BA6C 0218 9ABE 22CD 78C0 04AE 0337 2101 8B7C 26BB 32C5      STA2 2800
+
+            while (readText.Length != 0)
+            {
+                substringlength = readText.IndexOf(keystring);
+                if (substringlength != -1)
+                {
+                    count++;
+                    //s_time = readText.Substring(substringlength + keystring.Length + 184, 10);
+                    //新
+                    s_time = readText.Substring(substringlength + keystring.Length + 32, 10);
+                    s_time = s_time.Replace(" ", "");
+                    s_pv = readText.Substring(substringlength + keystring.Length + 42, 60);
+                    //新
+                    s_pv = s_pv.Replace(" ", "");
+                    s_px = s_pv.Substring(0, 8);
+                    s_py = s_pv.Substring(8, 8);
+                    s_pz = s_pv.Substring(16, 8);
+                    s_vx = s_pv.Substring(24, 8);
+                    s_vy = s_pv.Substring(32, 8);
+                    s_vz = s_pv.Substring(40, 8);
+                    sw.WriteLine(s_time + "\t" + s_px + "\t" + s_py + "\t" + s_pz + "\t" + s_vx + "\t" + s_vy + "\t" + s_vz);
+                    readText = readText.Remove(0, substringlength + keystring.Length - 1);
+                    progressBar1.Value = total_count - readText.Length;
+                }
+                else
+                    break;
+            }
+            progressBar1.Value = progressBar1.Maximum;
+            sw.Flush();
+            sw.Close();
+            richTextBox1.Text += count.ToString();
+
+            richTextBox1.Text += "数据提取完毕！";
+        }
+    }
+}
